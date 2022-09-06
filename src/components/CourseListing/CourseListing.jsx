7d012d51246c1e48f05c841e8096765010ed1896
Abbass-Hijazi40 <hijaziabbass40@gmail.com>
2022-09-06 15:15:11 +0300

    without login page

--- src/components/CourseListing/CourseListing.jsx
+++ src/components/CourseListing/CourseListing.jsx
@@ -4,7 +4,7 @@ import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import "../../assets/styles/CardListing.css";
-import "../../assets/styles/CardListing.css"
+
 import data from "../../components/CourseListing/data";
 import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
@@ -12,34 +12,28 @@ import Form from "react-bootstrap/Form";
 import SearchIcon from "../../assets/images/SearchIcon.png";
 import courseIcon from "../../assets/images/courseIcon.png";
 import { useState } from "react";
-
+import CardGroup from "react-bootstrap/CardGroup";
 
 export default function CourseListing() {
+  const [Filter, SetFilter] = useState("");
+  const searchText = (event) => {
+    SetFilter(event.target.value);
+  };
+  console.warn(Filter);
 
-    const [Filter, SetFilter] = useState("");
-    const searchText = (event) => {
-      SetFilter(event.target.value);
-    };
-    console.warn(Filter);
-
-    let dataSearch = data.cardData.filter((item) => {
-        return Object.keys(item).some((key) =>
-          item[key]
-            .toString()
-            .toLowerCase()
-            .includes(Filter.toString().toLowerCase())
-        );
-      });
-
-
+  let dataSearch = data.cardData.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key]
+        .toString()
+        .toLowerCase()
+        .includes(Filter.toString().toLowerCase())
+    );
+  });
 
   const renderCard = (card, index) => {
     return (
-      <Card
-        key={index}
-        style={{ width: "400px", height: "auto" }}
-        className="Hijazibox"
-      >
+      <Col>
+      <Card key={index} style={{ width: "400px", height: "auto" }} className="Cards">
         <Card.Img variant="top" src={card.img} style={{ height: "300px" }} />
         <Card.Body>
           <Card.Title>{card.title}</Card.Title>
@@ -53,26 +47,29 @@ export default function CourseListing() {
           <Button variant="primary">Enroll</Button>
         </Card.Body>
       </Card>
+      </Col>
     );
   };
 
   return (
     <>
       <Container fluid className="card-Listing-Container">
-        <Row style={{ height: "200px", textAlign: "center" }}>
+        <Row style={{ height: "auto", textAlign: "center" }}>
           <h1 style={{ marginTop: "100px" }}>Courses:</h1>
 
           <div style={{ textAlign: "center" }}>
             <InputGroup
               className="mb-3"
-              style={{ width: "500px", textAlign: "center" }}
+              style={{ width: "350px", textAlign: "center" ,margin:"auto",marginTop:"50px"}}
             >
               <InputGroup.Text>
                 <img src={courseIcon} alt="" style={{ height: "20px" }} />
               </InputGroup.Text>
-              <Form.Control aria-label="Amount (to the nearest dollar)" 
-              value={Filter}
-              onChange={searchText.bind(this)}
+              <Form.Control
+                aria-label="Amount (to the nearest dollar)"
+                value={Filter}
+                onChange={searchText.bind(this)}
+                
               />
               <InputGroup.Text>
                 <img src={SearchIcon} alt="" style={{ height: "20px" }} />
@@ -81,15 +78,26 @@ export default function CourseListing() {
           </div>
         </Row>
 
-       
 
-        <Row xs={1}>
-          <Col lg xl xxl={2}></Col>
-          <Col lg xl xxl={8}>
-            <div className="Hijazigrid">{dataSearch.map(renderCard)}</div>
-          </Col>
-          <Col lg xl xxl={2}></Col>
+        <Row xs={1} md={4} className="g-1" style={{marginTop:"50px"}}>
+          
+            
+              {dataSearch.map(renderCard)}
+           
+          
         </Row>
+
+        {/* <Row xs={1}>
+          <Col md={2} lg={2} xl={2} xxl={2}></Col>
+
+          <Col md={8} lg={8} xl={8} xxl={8}>
+            <div className="">
+              <CardGroup>{dataSearch.map(renderCard)}</CardGroup>
+            </div>
+          </Col>
+
+          <Col md={2} lg={2} xl={2} xxl={2}></Col>
+        </Row> */}
       </Container>
     </>
   );
